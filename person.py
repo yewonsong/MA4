2ee05a583739e88d768a26615e400576e35cae13
yewonsong <yewonsong01@naver.com>
2023-10-17 00:33:18 +0200

    braket add

--- person.py
+++ person.py
@@ -11,9 +11,9 @@ class Person(object):
 		lib.Person_get.restype = ctypes.c_int
 		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
 		lib.Person_fib.restype = ctypes.c_int
-        lib.Person__fib.argtypes = ctypes.c_int
-        lib.Person__fib.restype = ctypes.c_int 
-        lib.Person_delete.argtypes = [ctypes.c_void_p]
+        lib.Person__fib.argtypes = [ctypes.c_int]
+		lib.Person__fib.restype = ctypes.c_int 
+    	lib.Person_delete.argtypes = [ctypes.c_void_p]
 		self.obj = lib.Person_new(age)
 
 	def get(self):
@@ -21,6 +21,9 @@ class Person(object):
 
 	def set(self, age):
 		lib.Person_set(self.obj, age)
+	
+	def fib(self):
+		lib.Person_fib(self.obj)
         
 	def __del__(self):
 		return lib.Person_delete(self.obj)
